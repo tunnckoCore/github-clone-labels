e77e5683cd923a1f3c2dc4a4854cbad2c565def4
tunnckoCore <mameto_100@mail.bg>
2016-10-26 21:29:24 +0300

    chore(implement): init

---
+++ index.js
@@ -0,0 +1,53 @@
+/*!
+ * github-clone-labels <https://github.com/tunnckoCore/github-clone-labels>
+ *
+ * Copyright (c) 2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+var assert = require('assert')
+var ctrl = require('async')
+var get = require('simple-get')
+
+module.exports = function githubCloneLabels (opts, cb) {
+  assert.strictEqual(opts && typeof opts, 'object', 'expect `src` to be a object')
+  assert.strictEqual(typeof cb, 'function', 'expect `cb` to be a function')
+
+  var api = 'https://api.github.com/repos'
+  var headers = {
+    'user-agent': 'https://github.com/tunnckoCore/github-clone-labels',
+    authorization: opts.token ? `token ${opts.token}` : opts.auth
+  }
+
+  get.concat({
+    json: true,
+    headers: headers,
+    url: `${api}/${opts.src}/labels`
+  }, function callback (e, res, data) {
+    if (res.statusCode !== 200) {
+      var err = new Error(JSON.stringify(data))
+      err.res = res
+      cb(err)
+      return
+    }
+    ctrl.mapSeries(data, function (label, next) {
+      get.concat({
+        method: 'POST',
+        json: true,
+        headers: headers,
+        url: `${api}/${opts.dest}/labels`,
+        body: label
+      }, function (e, res, data) {
+        if (res.statusCode !== 201) {
+          var err = new Error(JSON.stringify(data))
+          err.res = res
+          next(err)
+          return
+        }
+        next(null, data, res)
+      })
+    }, cb)
+  })
+}
---
+++ test.js
@@ -0,0 +1,16 @@
+/*!
+ * github-clone-labels <https://github.com/tunnckoCore/github-clone-labels>
+ *
+ * Copyright (c) 2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+// githubCloneLabels({
+//   src: 'tunnckoCore/gruu',
+//   dest: 'tunnckoCore/gruu-reporter',
+//   token: 'fsdf'
+// }, function (err, data) {
+//   console.log(err, 'res', data)
+// })
